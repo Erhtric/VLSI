e43cd570f6a9b9964d9d4b6fa34db612f22fad80
WenXiaowei <xiaowei.wen1997@gmail.com>
2021-08-21 20:15:09 +0200

    adding solution time for cps models

--- src/minizinc/sol/sol_checker.py
+++ src/minizinc/sol/sol_checker.py
@@ -1,9 +1,9 @@
 from utilities.tools import *
 
-ROTATION = 1
+ROTATION = 0
 if __name__ == "__main__":
     # for k in range(1, 41):
-    for k in [10]:
+    for k in [11]:
         if ROTATION == 0:
             if k < 10:
                 file = f"./sol-0{k}.txt"
@@ -16,7 +16,7 @@ if __name__ == "__main__":
             if k < 10:
                 file = f"./sol-0{k}-rot.txt"
             else:
-                file = f"./sol-{k}-rot.txt"
+                file = f"./sol-{k}.txt"
             dim, n_circuits, coordinates = read_solution_parameters(file)
             sat = check_sat_rot(dim, coordinates)
             print(f"{k} Sat? {sat}")
--- src/minizinc/sol/solution_to_picture.py
+++ src/minizinc/sol/solution_to_picture.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
         out_path = sys.argv[2]
     files = os.listdir(in_path)
     heights = {}
-    sol_n = 18
+    sol_n = 1
     files = [f"sol-0{sol_n}.txt" for sol_n in range(1,10)]
     for f in files:
         obj = re.search("^sol-[0-9]+.txt", f)
@@ -21,7 +21,7 @@ if __name__ == "__main__":
             dim, n_circuits, shapes = read_solution_parameters(f)
             heights[f] = dim[1]
             solution = draw_solution(dim, shapes)
-            # print(solution)
+            print(solution)
             new_f = f.replace("txt", "png")
             show_shape(solution, new_f, n_circuits,shapes)
     print(heights)
